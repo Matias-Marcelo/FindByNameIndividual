8f9678f9e1a14998eb4116d5888ff8a15b1580f4
Matias-Marcelo <166701097+Matias-Marcelo@users.noreply.github.com>
2024-10-10 16:02:56 +0200

    First Commit

---
+++ src/main/java/com/example/findByName/FindByNameApplication.java
@@ -0,0 +1,13 @@
+package com.example.findByName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindByNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindByNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/findByName/FindByNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findByName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindByNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
